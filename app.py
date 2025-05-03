b0d6c9c9f832d881c29a7e7148ec2e23795a2262
Venkatesh Mudaliar <venkateshmudaliar@venkateshs-mbp.mynetworksettings.com>
2025-05-03 03:42:54 -0400

    Restored app and config files

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import pandas as pd
 import plotly.express as px
 
 # Load and prepare dataset
-df = pd.read_csv("Downloads/NYPD_Arrests_Data__Historic__2020-2025.csv")
+df = pd.read_csv("https://drive.google.com/uc?id=1Oludb5WpBaL-dkXwh4g0HBGnt_qUR3y1")
+
 df = df[df['LAW_CAT_CD'] == 'F']
 df = df.dropna(subset=['Latitude', 'Longitude', 'OFNS_DESC', 'ARREST_BORO'])
 df['ARREST_DATE'] = pd.to_datetime(df['ARREST_DATE'], errors='coerce')
